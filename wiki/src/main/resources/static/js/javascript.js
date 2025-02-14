a57bb6c6560cd261d0ec9624e8d3bf3317ce29cd
Juan Martín Sánchez <jmartin_sanchez@javeriana.edu.co>
2025-02-14 15:19:29 -0500

    Correcion formulario html, javascript completado envio de correo

--- wiki/src/main/resources/static/js/javascript.js
+++ wiki/src/main/resources/static/js/javascript.js
@@ -1,5 +1,30 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) => {
+  e.preventDefault()
+  
+  // serviceID - templetaID - #form - publickKey
+
+  emailjs.sendForm('service_u4fv0on', 'template_7tvajdd', '#contact-form', 'vuJXAOpNxn8kVtrUj')
+
+  .then(() => {
+    //Mensaje enviado
+    contactMessage.textContent = 'Mensaje enviado correctamente ✅'
+
+    //Borrar mensaje 5 segundos despues
+    setTimeout(() => {
+      contactMessage.textContent = ''
+    }, 5000)
+
+    contactForm.reset()
+}, () => {
+    //Mensaje de error
+    contactMessage.textContent = 'Error al enviar el mensaje ❌'
+    })
+}
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
