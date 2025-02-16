33a593d6a469c1f84e9ded4427ea3b1d9533d7b6
Juan Martín Sánchez <jmartin_sanchez@javeriana.edu.co>
2025-02-15 22:40:21 -0500

    Finalizacion pantalla de inicio, actualizacion botones intergrantes y controladoes

--- wiki/src/main/java/co/edu/javeriana/wiki/controllers/inicioController.java
+++ wiki/src/main/java/co/edu/javeriana/wiki/controllers/inicioController.java
@@ -7,14 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
 
 
-
-
-
-
 @Controller
 public class inicioController {
-    @GetMapping("/index")
+    @GetMapping("/")
     public String inicio() {
-        return "index";
+        return "PaginaInicio";
     }
+    @GetMapping("PaginaInicio")
+    public String inicioReturn() {
+        return "PaginaInicio";
+    }
+    
 }
--- wiki/src/main/java/co/edu/javeriana/wiki/controllers/integrantesController.java
+++ wiki/src/main/java/co/edu/javeriana/wiki/controllers/integrantesController.java
@@ -3,12 +3,22 @@ package co.edu.javeriana.wiki.controllers;
 //Controlador de los integrantes del proyecto
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-
-
-
+import java.util.List;
 
 @Controller
+@RequestMapping("/")
 public class integrantesController {
+    private final List<String> validNames = List.of("Juan", "Juano", "Gaby", "Carva", "Luis", "Miguel");
 
+    @GetMapping("{name}")
+    public String loadPage(@PathVariable String name) {
+        if (!validNames.contains(name)) {
+            return "error"; // Si el nombre no está en la lista, redirige a una página de error
+        }
+        return name;
+    }
 }
