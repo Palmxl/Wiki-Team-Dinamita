12e6f389de3a1d18661fef2884e5bfc4114b51ea
Juan Martín Sánchez <jmartin_sanchez@javeriana.edu.co>
2025-02-16 03:12:19 -0500

    Correcion links proyectos

--- wiki/src/main/java/co/edu/javeriana/wiki/controllers/integrantesController.java
+++ wiki/src/main/java/co/edu/javeriana/wiki/controllers/integrantesController.java
@@ -13,12 +13,12 @@ import java.util.List;
 @RequestMapping("/")
 public class integrantesController {
     private final List<String> validNames = List.of("Juan", "Juano", "Gaby", "Carva", "Luis", "Miguel");
-
+    //private final List<String> validProjects = List.of("index");
     @GetMapping("{name}")
     public String loadPage(@PathVariable String name) {
         if (!validNames.contains(name)) {
             return "error"; // Si el nombre no está en la lista, redirige a una página de error
         }
         return name;
-    }
+    }    
 }
